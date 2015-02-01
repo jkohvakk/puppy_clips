991a481840e18867f48a547aaa4a654fdb8d1c40
Janne Kohvakka <jkohvakk@gmail.com>
2015-02-01 13:21:15 +0200

    Automatic conversion to mp4

--- src/puppyclips.py
+++ src/puppyclips.py
@@ -1,6 +1,8 @@
 import time
 import movementsensor
 import picamera
+import sys
+import subprocess
 
 
 class PuppyClips(object):
@@ -9,9 +11,10 @@ class PuppyClips(object):
         self._movementsensor = movementsensor.MovementSensor()
         self._camera = picamera.PiCamera()
 
-    def run(self, round_count='infinite'):
+    def run(self, round_count='infinite', max_clips=60):
         loop_count = round_count if round_count != 'infinite' else 1
-        while loop_count > 0:
+        self._clips = 0
+        while loop_count > 0 and self._clips < max_clips:
             time.sleep(1)
             if self._movementsensor.is_movement():
                 self.take_clip()
@@ -19,11 +22,16 @@ class PuppyClips(object):
                 loop_count -= 1
 
     def take_clip(self):
-        self._camera.start_recording('{}.h264'.format(time.strftime('%Y.%m.%d-%H:%M')))
+        name = '{}'.format(time.strftime('%Y.%m.%d-%H:%M'))
+        self._camera.start_recording('{}.h264'.format(name))
         time.sleep(60)
         self._camera.stop_recording()
+        subprocess.call(['MP4Box', '-fps', '30', '-add',
+                         '{}.h264'.format(name),
+                         '{}.mp4'.format(name)])
+        self._clips += 1
 
 
 if __name__ == '__main__':
     pc = PuppyClips()
-    pc.run('infinite')
+    pc.run(round_count='infinite', max_clips=sys.argv[1])
--- tst/test_puppy_clips.py
+++ tst/test_puppy_clips.py
@@ -35,21 +35,25 @@ class MockMovementSensor(SelfTrackingMock):
 
 class MockPiCamera(SelfTrackingMock):
 
-    def start_recording(self, *args):
-        if hasattr(self, 'start_recording_args'):
-            assert('Only support one call for now')
-        self.start_recording_args = args
+    def __init__(self):
+        super(MockPiCamera, self).__init__()
+        self.start_recording_calls = []
+        self.stop_recording_calls = []
 
-    def start_recording_called_once_with(self, *args):
-        return self.start_recording_args == args
+    def start_recording(self, *args):
+        self.start_recording_calls.append(args)
 
     def stop_recording(self, *args):
-        if hasattr(self, 'stop_recording_args'):
-            assert('Only support one call for now')
-        self.stop_recording_args = args
+        self.stop_recording_calls.append(args)
 
-    def stop_recording_called_once_with(self, *args):
-        return self.stop_recording_args == args
+
+class MockSubprocess(object):
+
+    call_calls = []
+
+    @classmethod
+    def call(cls, args):
+        cls.call_calls.append(args)
 
 
 class TestPuppyClips(unittest.TestCase):
@@ -77,8 +81,24 @@ class TestPuppyClips(unittest.TestCase):
         self.pc.run(3)
         self.assertEqual(mock_time.get_mock_sleep_calls(), 4)
         self.assertEqual(mock_time.get_mock_sleep_times(), [1, 1, 60, 1])
-        self.assertTrue(MockPiCamera.get_instances()[0].start_recording_called_once_with('2015.01.31-20:10.h264'))
-        self.assertTrue(MockPiCamera.get_instances()[0].stop_recording_called_once_with())
+        self.assertEqual(MockPiCamera.get_instances()[0].start_recording_calls[0],
+                         ('2015.01.31-20:10.h264',))
+        self.assertEqual(1, len(MockPiCamera.get_instances()[0].stop_recording_calls))
+
+    def test_it_is_possible_to_set_max_clips(self):
+        self.pc._movementsensor.set_movement([True, True, True])
+        self.pc.run(3, max_clips=2)
+        self.assertEqual(2, len(MockPiCamera.get_instances()[0].start_recording_calls))
+        self.assertEqual(2, len(MockPiCamera.get_instances()[0].stop_recording_calls))
+        self.assertEqual(mock_time.get_mock_sleep_calls(), 4)
+
+    def test_clip_is_converted_from_h264_to_mp4(self):
+        puppyclips.subprocess = MockSubprocess
+        self.pc._movementsensor.set_movement([False, True, False])
+        self.pc.run(3)
+        self.assertEqual(MockSubprocess.call_calls, [['MP4Box', '-fps', '30', '-add',
+                                                      '2015.01.31-20:10.h264', '2015.01.31-20:10.mp4']])
+
 
 
 if __name__ == "__main__":
