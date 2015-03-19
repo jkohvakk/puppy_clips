a50d5dde3b82c7b0f9131dd67060dc3c9195ccf9
Janne Kohvakka <jkohvakk@gmail.com>
2015-03-19 21:23:53 +0200

    On green again

--- tst/test_puppy_clips.py
+++ tst/test_puppy_clips.py
@@ -1,7 +1,7 @@
 import unittest
 import puppyclips
 import mock_time
-from mock import patch, Mock
+from mock import patch, Mock, call
 
 
 class SelfTrackingMock(object):
@@ -21,35 +21,6 @@ class SelfTrackingMock(object):
         self._calls = []
 
 
-class MockPiCamera(SelfTrackingMock, unittest.TestCase):
-
-    def __init__(self, framerate=1):
-        super(MockPiCamera, self).__init__()
-        self.start_recording_calls = []
-        self.stop_recording_calls = []
-        if framerate != 20:
-            self.fail('Expected framerate to be 20')
-
-    def start_recording(self, *args):
-        self.start_recording_calls.append(args)
-
-    def stop_recording(self, *args):
-        self.stop_recording_calls.append(args)
-
-
-class MockSubprocess(object):
-
-    call_calls = []
-
-    @classmethod
-    def call(cls, args):
-        cls.call_calls.append(args)
-
-    @classmethod
-    def reset(cls):
-        cls.call_calls = []
-
-
 class MockOs(object):
 
     remove_calls = []
@@ -59,17 +30,17 @@ class MockOs(object):
         cls.remove_calls.append(filename)
 
 
-
 class TestPuppyClips(unittest.TestCase):
 
     def setUp(self):
-        MockPiCamera.reset()
-        MockSubprocess.reset()
         puppyclips.movementsensor.RPi = Mock()
-        puppyclips.picamera.PiCamera = MockPiCamera
         puppyclips.time = mock_time
         mock_time.reset()
+        self.picamera_class_mock = Mock()
+        puppyclips.picamera.PiCamera = self.picamera_class_mock
         self.pc = puppyclips.PuppyClips()
+        self.picamera_class_mock.assert_called_with(framerate=20)
+        self.picamera = self.picamera_class_mock.return_value
 
     @patch('puppyclips.movementsensor.MovementSensor.is_movement')
     def test_movement_sensor_is_polled_every_second(self, is_movement):
@@ -87,44 +58,54 @@ class TestPuppyClips(unittest.TestCase):
         self.movements = movements
         mock_is_movement.side_effect = self.get_movements
 
+    @patch('puppyclips.subprocess')
     @patch('puppyclips.movementsensor.MovementSensor.is_movement')
-    def test_if_movement_a_minute_clip_is_taken_with_name_based_on_current_datetime(self, is_movement):
+    def test_if_movement_a_minute_clip_is_taken_with_name_based_on_current_datetime(self,
+                                                                                    is_movement,
+                                                                                    subprocess):
         puppyclips.os = MockOs
-        puppyclips.subprocess = MockSubprocess
         self.set_mock_movements([False, True, False], is_movement)
         self.pc.run(3)
         self.assertEqual(mock_time.get_mock_sleep_calls(), 4)
         self.assertEqual(mock_time.get_mock_sleep_times(), [1, 1, 60, 1])
-        self.assertEqual(MockPiCamera.get_instances()[0].start_recording_calls[0],
-                         ('2015.01.31-20:10.h264',))
-        self.assertEqual(1, len(MockPiCamera.get_instances()[0].stop_recording_calls))
+        self.picamera.start_recording.assert_called_once_with('2015.01.31-20:10.h264')
+        self.picamera.stop_recording.assert_called_once_with()
 
+    @patch('puppyclips.subprocess')
     @patch('puppyclips.movementsensor.MovementSensor.is_movement')
-    def test_it_is_possible_to_set_max_clips(self, is_movement):
+    def test_it_is_possible_to_set_max_clips(self, is_movement, subprocess):
         self.set_mock_movements([True, True, True], is_movement)
         self.pc.run(3, max_clips=2)
-        self.assertEqual(2, len(MockPiCamera.get_instances()[0].start_recording_calls))
-        self.assertEqual(2, len(MockPiCamera.get_instances()[0].stop_recording_calls))
+        expected_picamera_mock_calls = [call.start_recording('2015.01.31-20:10.h264'),
+                                        call.stop_recording(),
+                                        call.start_recording('2015.01.31-20:10.h264'),
+                                        call.stop_recording()]
+        self.assertEqual(expected_picamera_mock_calls, self.picamera.mock_calls)
         self.assertEqual(mock_time.get_mock_sleep_calls(), 4)
 
+    @patch('puppyclips.subprocess')
     @patch('puppyclips.movementsensor.MovementSensor.is_movement')
-    def test_clip_is_converted_from_h264_to_mp4(self, is_movement):
-        puppyclips.subprocess = MockSubprocess
+    def test_clip_is_converted_from_h264_to_mp4(self, is_movement, subprocess):
         puppyclips.os = MockOs
         self.set_mock_movements([False, True, False], is_movement)
         self.pc.run(3)
-        self.assertEqual(MockSubprocess.call_calls[0], ['MP4Box', '-fps', '20', '-add',
-                                                      '2015.01.31-20:10.h264', '2015.01.31-20:10.mp4'])
+        self.assertEqual(call(['MP4Box',
+                               '-fps', '20',
+                               '-add', '2015.01.31-20:10.h264',
+                               '2015.01.31-20:10.mp4']), subprocess.call.mock_calls[0])
         self.assertEqual(MockOs.remove_calls[0], '2015.01.31-20:10.h264')
 
+    @patch('puppyclips.subprocess')
     @patch('puppyclips.movementsensor.MovementSensor.is_movement')
-    def test_clip_is_uploaded_to_dropbox(self, is_movement):
-        puppyclips.subprocess = MockSubprocess
+    def test_clip_is_uploaded_to_dropbox(self, is_movement, subprocess):
+        puppyclips.os = MockOs
         self.set_mock_movements([False, True, False], is_movement)
         self.pc.run(3)
-        self.assertEqual(MockSubprocess.call_calls[1], ['/home/pi/Dropbox-Uploader/dropbox_uploader.sh', 'upload',
-                                                        '2015.01.31-20:10.mp4', '2015.01.31-20:10.mp4'])
-
+        self.assertEqual(call(['/home/pi/Dropbox-Uploader/dropbox_uploader.sh',
+                               'upload',
+                               '2015.01.31-20:10.mp4',
+                               '2015.01.31-20:10.mp4']),
+                         subprocess.call.mock_calls[1])
 
 
 if __name__ == "__main__":
